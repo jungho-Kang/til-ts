095e78e6e5b00f55ba10c376ab35f0d149a14aa2
jungho-Kang <rkdwjdgh08@gmail.com>
2025-01-21 12:48:19 +0900

    [docs] 기본타입

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
-const age = 200;
+// 배열
+const arr = [1, 2, 3];
+arr.push(4);
+arr.push(5);
+arr.push(6);
+console.log(arr);
+// 튜플
+const arrT = [1, 2, 3];
+arrT.push(4);
+arrT.push(5);
+arrT.push(6);
+console.log(arrT);
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-const age: number = 200;
+// 비동기 함수
+async function fetchGetTodo(): Promise<void> {
+  const res = await fetch("주소");
+}
+async function fetchGetTodoOne(): Promise<string> {
+  const res = await fetch("주소");
+  return "hello";
+}
+async function fetchPostTodo(): Promise<boolean> {
+  const res = await fetch("주소");
+  return true;
+}
+type Todo = {
+  id: number;
+  title: string;
+};
+async function fetchSortTodo(): Promise<Todo> {
+  const res = await fetch("주소");
+  return { id: 1, title: "안녕" };
+}

dcce5fbafff591783a306faaa6f11a1caa0899b8
jungho-Kang <rkdwjdgh08@gmail.com>
2025-01-23 10:12:41 +0900

    [docs] tag 유니온

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-type Person = {
-  name: string;
-  age: number;
-};
-
-function func(value: string | number | Date | null | Person) {
-  if (typeof value === "string") {
-    value.toUpperCase();
-  } else if (typeof value === "number") {
-    value.toFixed(2);
-  } else if (value instanceof Date) {
-    // Date라는 것을 보장받음
-    value.getTime();
-  } else if ("age" in (value as Person)) {
-    console.log((value as Person).age);
+type Cat = { kind: "CAT"; sound: string; color: string };
+type Dog = { kind: "DOG"; sound: string; food: string };
+type Bird = { kind: "BIRD"; sound: string; fly: boolean };
+type Animal = Cat | Dog | Bird;
+// 동물의 소리를 출력하는 기능
+function song(what: Animal) {
+  switch (what.kind) {
+    case "CAT":
+      console.log(`고양이 울음소리 : ${what.sound}`);
+      break;
+    case "DOG":
+      console.log(`강아지  울음소리 : ${what.sound}`);
+      break;
+    case "BIRD":
+      console.log(`새  울음소리 : ${what.sound}`);
+      break;
   }
 }
